575629b0d9a7c101b874e054baf3fc402f8a4e2e
Raj Seshadri <sanstorage@gmail.com>
2019-12-05 14:38:40 -0700

    Update main.go

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ type Welcome struct {
 func main() {
    //Instantiate a Welcome struct object and pass in some random information. 
    //We shall get the name of the user as a query parameter from the URL
-   welcome := Welcome{"Folks", time.Now().Format(time.Stamp)}
+   welcome := Welcome{"Denver", time.Now().Format(time.Stamp)}
 
    //We tell Go exactly where we can find our html file. We ask Go to parse the html file (Notice
    // the relative path). We wrap it in a call to template.Must() which handles any errors and halts if there are fatal errors
